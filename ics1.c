4ca971232ad7c0ae7fd90efb6b8b9092107e0d22
Ratul Bhuiyan <143863186+RatulInnovates@users.noreply.github.com>
2023-10-11 04:16:36 +0000

    escape sequences

--- ics1.c
+++ ics1.c
@@ -1,12 +1,16 @@
 //To learn programming: 1. learn syntax. 2. logic
 // c is a case sensitive language
 // there are 32 keywords in C
-// 
+// "\t = new tab"
+/*
+
+
+*/
 
 
 
 #include<stdio.h>
-int main()
+void main()
 {
-    printf("hdflkald;\n");
+    printf("hd\000flkald;\n");
 }
