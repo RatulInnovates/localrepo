46a18e09ceb287c8c0241cce365d3fc4f93a91bc
Ratul Bhuiyan <143863186+RatulInnovates@users.noreply.github.com>
2023-10-11 03:32:35 +0000

    class notes

---
+++ ics1.c
@@ -0,0 +1,12 @@
+//To learn programming: 1. learn syntax. 2. logic
+// c is a case sensitive language
+// there are 32 keywords in C
+// 
+
+
+
+#include<stdio.h>
+int main()
+{
+    printf("hdflkald;\n");
+}
